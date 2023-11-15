c436286f9ac322e9926b9a451400a0492d0239fe
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2023-11-15 12:48:30 -0600

    Added Setter Inject and Constructor Injection XML examples.

--- conference-xml/src/main/java/org/fatalzio/service/SpeakerServiceImpl.java
+++ conference-xml/src/main/java/org/fatalzio/service/SpeakerServiceImpl.java
@@ -2,16 +2,26 @@ package org.fatalzio.service;
 
 import org.fatalzio.model.Speaker;
 import org.fatalzio.repository.HibernateSpeakerRepository;
-import org.fatalzio.repository.HibernateSpeakerRepositoryImpl;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private final HibernateSpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private HibernateSpeakerRepository repository;
+
+    public SpeakerServiceImpl() {
+    }
+
+    public SpeakerServiceImpl(HibernateSpeakerRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
+    }
+
+    public void setSpeakerRepository(HibernateSpeakerRepository repository) {
+        this.repository = repository;
     }
 }
